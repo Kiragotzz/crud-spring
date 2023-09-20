8852035315402287a5b7d6be94461ad6921b8545
Luís Gabriel Gomes da Silva <luis_gabriell1@hotmail.com>
2023-09-20 06:08:59 -0300

    trat var _id from entity with jsonproperty

---
+++ src/main/java/com/luis/crudspring/controller/CourseController.java
@@ -0,0 +1,33 @@
+package com.luis.crudspring.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.luis.crudspring.model.Course;
+import com.luis.crudspring.repository.CourseRepository;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping("/api/courses")
+@AllArgsConstructor
+public class CourseController {
+
+  private final CourseRepository courseRepository;
+
+  // @RequestMapping(method = RequestMethod.GET)
+  @GetMapping
+  public @ResponseBody List<Course> list(){
+    return courseRepository.findAll();
+  }
+
+
+}
