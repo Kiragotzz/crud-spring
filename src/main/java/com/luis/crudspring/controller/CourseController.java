e493d5a28981f67bd67ca43094f6d1110ab4c2fa
Luís Gabriel Gomes da Silva <luis_gabriell1@hotmail.com>
2023-10-06 06:45:28 -0300

    post endpoint to save new course from angular front-end

--- src/main/java/com/luis/crudspring/controller/CourseController.java
+++ src/main/java/com/luis/crudspring/controller/CourseController.java
@@ -5,10 +5,16 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.luis.crudspring.model.Course;
@@ -25,9 +31,18 @@ public class CourseController {
 
   // @RequestMapping(method = RequestMethod.GET)
   @GetMapping
-  public @ResponseBody List<Course> list(){
+  public @ResponseBody List<Course> list() {
     return courseRepository.findAll();
   }
 
+  // @RequestMapping(method= RequestMethod.POST)
+  @PostMapping
+  @ResponseStatus(code=HttpStatus.CREATED)
+  public Course create(@RequestBody Course course) {
+    // System.out.println(course.getName());
+    return courseRepository.save(course);
+    // return ResponseEntity.status(HttpStatus.CREATED)
+    // .body(courseRepository.save(course));
+  }
 
 }
